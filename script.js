be06aa5eebd68a9e83b851656bf87ebc039dae99
Deepak0090 <167174586+Deepak0090@users.noreply.github.com>
2025-01-17 20:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+() => {
+    cy.get('h1').should('have.length', 1);  // Only one <h1> element
+    cy.get('.testimonial').should('have.length', 3);  // There are 3 testimonial divs
+    cy.get('p').should('have.length', 6);  // There are 6 <p> elements (3 testimonial text + 3 author)
+}
